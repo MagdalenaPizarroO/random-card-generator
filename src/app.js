62c9482073a6221a888e1ce39167e54340485880
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-02-27 15:52:03 +0000

    tiene un error hmmmm

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function Numero() {
+  let ArrNum = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNum = Math.floor(Math.random() * 13);
+  return ArrNum[randomNum];
+}
+
+function crearPinta() {
+  let ArrPinta = ["♥", "♠", "♦", "♣"];
+  if (ArrPinta === "♥" || ArrPinta === "♦") {
+    document.querySelector("#top").style.color = "red";
+    document.querySelector("#bottom").style.color = "red";
+  }
+  return ArrPinta[index];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#center").innerHTML = Numero(Index);
+  let randomPinta = Math.floor(Math.random() * 4);
+  let pinta = crearPinta(randomPinta);
+
+  document.querySelector("top").innerHTML = pinta;
+  document.querySelector("bottom").innerHTML = pinta;
 };
