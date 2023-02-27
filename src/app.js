a3c37e2a484d1ee2c24605aeebea7632e8041c48
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-02-27 22:11:22 +0000

    quedó hermoso

--- src/app.js
+++ src/app.js
@@ -42,3 +42,28 @@ window.onload = function() {
   document.querySelector("#top").innerHTML = pinta;
   document.querySelector("#bottom").innerHTML = pinta;
 };
+
+function generarCarta() {
+  document.querySelector("#center").innerHTML = Numero();
+  const pinta = crearPinta();
+  if (pinta === "♥" || pinta === "♦") {
+    document.querySelector("#center").style.color = "red";
+  } else {
+    document.querySelector("#center").style.color = "black";
+    document.querySelector("#top").style.color = "black";
+    document.querySelector("#bottom").style.color = "black";
+  }
+  document.querySelector("#top").innerHTML = pinta;
+  document.querySelector("#bottom").innerHTML = pinta;
+}
+
+//para crear el botón
+const buttonContainer = document.createElement("div");
+buttonContainer.style.textAlign = "center";
+
+const button = document.createElement("button");
+button.innerHTML = "Give me a new card!";
+button.addEventListener("click", generarCarta);
+
+buttonContainer.appendChild(button);
+document.body.appendChild(buttonContainer);
